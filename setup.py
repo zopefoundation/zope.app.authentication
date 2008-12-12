92c8077a08c0154119ef7c6d36cdcddb1e4bbc86
Dan Korostelev <nadako@gmail.com>
2008-12-12 08:59:54 +0000

    Version bump.

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.authentication',
-      version = '3.4.4dev',
+      version = '3.5.0dev',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Pluggable Authentication Utility',
